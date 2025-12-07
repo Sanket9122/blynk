254b8740e29d4b31ed3b3979567a60a97453e41b
Sanket9122 <priyadarshisanket128@gmail.com>
2025-12-08 01:40:09 +0530

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 // MongoDB connect (no extra options needed now)
